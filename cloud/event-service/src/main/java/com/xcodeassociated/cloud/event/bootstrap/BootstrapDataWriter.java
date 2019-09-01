b9457889dd437cc081f185f3c537a53de7ef8838
XcodeAssociated <janusz.majchrzak@student.uj.edu.pl>
2019-09-01 14:58:51 +0200

    Add profile annotation for bootstrap data writter

--- cloud/event-service/src/main/java/com/xcodeassociated/cloud/event/bootstrap/BootstrapDataWriter.java
+++ cloud/event-service/src/main/java/com/xcodeassociated/cloud/event/bootstrap/BootstrapDataWriter.java
@@ -5,11 +5,13 @@ import com.xcodeassociated.cloud.event.repository.ReservationRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
 @Log4j2
 @Component
+@Profile("dev")
 public class BootstrapDataWriter implements ApplicationRunner {
     private final ReservationRepository reservationRepository;
 
