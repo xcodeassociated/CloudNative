7066834857f91d7f50063769de118bcc66a3b677
xcodeassociated <janusz.majchrzak@student.uj.edu.pl>
2019-09-23 01:36:53 +0200

    Use postgreSQL as production db for user-service

--- cloud/user-service/src/main/java/com/xcodeassociated/cloud/user/model/User.java
+++ cloud/user-service/src/main/java/com/xcodeassociated/cloud/user/model/User.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
